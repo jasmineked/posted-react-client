376ec24c274b8db895a0c0f6f7bd0d70bc504b29
jasmineked <jked630@icloud.com>
2020-12-01 10:17:15 -0500

    feat: index uploads component
    Co-authored-by: Nathan nathan.gundy@gmail.com

--- src/components/Index/Index.js
+++ src/components/Index/Index.js
@@ -1,3 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+
+import apiUrl from '../../apiConfig'
+
+const Uploads = props => () {
+  const [uploads, setUploads] = useState([])
+
+  useEffect(() => {
+    axios(`${apiUrl}/uploads`)
+    .then(res => setUploads(res.data.uploads))
+    .catch(console.error)
+  }, [])
+
+  // // const uploadsJsx = uploads.map(upload => (
+  //   // <li key={upload}>
+  //
+  // ))
+
+  return (
+    <div>
+      <h2>Uploads</h2>
+    </div>
+  )
+}
